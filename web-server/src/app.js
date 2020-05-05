642f1b8371f54188077634d43bf1094ad8663490
mc-santiago <mari.c.santiago@europe.com>
2020-05-06 00:36:28 +0200

    accessing api from browser and wiring up the user interface

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
 const app = express();
 
@@ -41,16 +43,29 @@ app.get("/help", (req, res) => {
 
 // app.com/weather
 app.get("/weather", (req, res) => {
-  res.send([
-    {
-      forecast: 30,
-      location: "Madrid",
-    },
-    {
-      forecast: 25,
-      location: "London",
-    },
-  ]);
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address.",
+    });
+  }
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({ error });
+        }
+        res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address,
+        });
+      });
+    }
+  );
 });
 
 app.get("/help/*", (req, res) => {
