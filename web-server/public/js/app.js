a6372e609b0b1a269d7978135272904a6b626a05
mc-santiago <mari.c.santiago@europe.com>
2020-05-06 00:37:17 +0200

    small problem fixed

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -18,7 +18,7 @@ weatherForm.addEventListener("submit", (e) => {
           messageOne.textContent = "There has been an error.";
         } else {
           messageOne.textContent = `Location: ${data.location}`;
-          messageTwo.textContent = `Forecast: ${data.forecast};
+          messageTwo.textContent = `Forecast: ${data.forecast}`;
         }
       });
     }
